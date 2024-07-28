f2d620e7654d9a7e39890a52469b955cc13d545b
VIGNESH VASU <vigneshvasu0511@gmail.com>
2024-07-28 20:04:20 +0530

    logo

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { ModeToggle } from "../mode-toggle";
+import logoImage from "/src/assets/inventory-management.png"; // Adjust the path to your logo image
 
 const Navbar = () => {
   const NavLinks = [
@@ -13,7 +14,10 @@ const Navbar = () => {
 
   return (
     <nav className="w-full z-50 top-0 h-16 bg-gray-800 text-white shadow-md flex items-center px-6 fixed">
-      <div className="text-2xl font-bold flex-1">Invkeep</div>
+      <div className="flex items-center space-x-4">
+        <img src={logoImage} alt="Logo" className="h-10 w-10" />
+        <span className="text-2xl font-bold">Invkeep</span>
+      </div>
       <ul className="flex items-center space-x-6 flex-1 justify-end px-40 gap-9">
         {NavLinks.map((link, index) => (
           <li key={index}>
