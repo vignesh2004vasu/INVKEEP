4dc25815a0cc65fac32401fe55d90bf4b5322598
VIGNESH VASU <vigneshvasu0511@gmail.com>
2024-07-30 20:41:20 +0530

    update graphs

--- src/pages/Shared/Home.jsx
+++ src/pages/Shared/Home.jsx
@@ -1,55 +1,40 @@
-"use client"
+"use client";
 
-import { useUser } from '@/components/UserContext'; // Adjust path as needed
-import { TrendingUp } from "lucide-react"
-import { Bar, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer, PieChart, Pie, Cell, LineChart, Line, ComposedChart } from "recharts"
-import { useSpring, animated } from '@react-spring/web';
+import { useEffect, useState } from "react";
+import { useUser } from "@/components/UserContext";
 import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+  Bar,
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  Cell,
+  LineChart,
+  Line,
+  ComposedChart,
+} from "recharts";
+import { useSpring, animated } from "@react-spring/web";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { RadialChart } from "./RadialChart";
+import  PieNew  from "../Shared/Pie";
+import BarNew from "./BarNew";
+import LineNew from "./LineNew";
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+const fetchData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
 
 const numberOfProducts = 120
 const numberOfSales = 300
 const totalRevenue = 15000
-
-const salesData = [
-  { month: "January", sales: 400 },
-  { month: "February", sales: 300 },
-  { month: "March", sales: 500 },
-  { month: "April", sales: 700 },
-  { month: "May", sales: 600 },
-  { month: "June", sales: 800 },
-]
-
-const revenueData = [
-  { month: "January", revenue: 4000 },
-  { month: "February", revenue: 3500 },
-  { month: "March", revenue: 4500 },
-  { month: "April", revenue: 5000 },
-  { month: "May", revenue: 4700 },
-  { month: "June", revenue: 6000 },
-]
-
-const productSalesData = [
-  { name: "Product A", value: 400 },
-  { name: "Product B", value: 300 },
-  { name: "Product C", value: 300 },
-  { name: "Product D", value: 200 },
-]
-
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"]
-
-const inventoryData = [
-  { name: "January", inventory: 400, sales: 240, revenue: 2400 },
-  { name: "February", inventory: 300, sales: 456, revenue: 1398 },
-  { name: "March", inventory: 300, sales: 139, revenue: 2210 },
-  { name: "April", inventory: 200, sales: 980, revenue: 2290 },
-  { name: "May", inventory: 278, sales: 390, revenue: 2000 },
-  { name: "June", inventory: 189, sales: 480, revenue: 2181 },
-]
 
 const AnimatedNumber = ({ number }) => {
   const { number: animatedNumber } = useSpring({
@@ -60,16 +45,22 @@ const AnimatedNumber = ({ number }) => {
   return <animated.span>{animatedNumber.to(n => n.toFixed(0))}</animated.span>
 };
 
+
 export default function Home() {
   const { user } = useUser();
+
 
   return (
     <div className="container mx-auto py-8 mt-16">
       <div className="mb-8">
         {user ? (
-          <h1 className="text-3xl font-bold mb-4">Welcome back, {user.firstName}!</h1>
+          <h1 className="text-3xl font-bold mb-4">
+            Welcome back, {user.firstName}!
+          </h1>
         ) : (
-          <h1 className="text-3xl font-bold mb-4">Welcome to the Inventory Dashboard!</h1>
+          <h1 className="text-3xl font-bold mb-4">
+            Welcome to the Inventory Dashboard!
+          </h1>
         )}
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
@@ -78,7 +69,9 @@ export default function Home() {
             <CardTitle>Number of Products</CardTitle>
           </CardHeader>
           <CardContent>
-            <p className="text-2xl font-bold"><AnimatedNumber number={numberOfProducts} /></p>
+            <p className="text-2xl font-bold">
+              <AnimatedNumber number={numberOfProducts} />
+            </p>
           </CardContent>
         </Card>
         <Card>
@@ -86,7 +79,9 @@ export default function Home() {
             <CardTitle>Number of Sales</CardTitle>
           </CardHeader>
           <CardContent>
-            <p className="text-2xl font-bold"><AnimatedNumber number={numberOfSales} /></p>
+            <p className="text-2xl font-bold">
+              <AnimatedNumber number={numberOfSales} />
+            </p>
           </CardContent>
         </Card>
         <Card>
@@ -94,25 +89,20 @@ export default function Home() {
             <CardTitle>Total Revenue</CardTitle>
           </CardHeader>
           <CardContent>
-            <p className="text-2xl font-bold">$<AnimatedNumber number={totalRevenue} /></p>
+            <p className="text-2xl font-bold">
+              $<AnimatedNumber number={totalRevenue} />
+            </p>
           </CardContent>
         </Card>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
-        <Card className='rounded-xl'>
+        <Card className="rounded-xl">
           <CardHeader>
             <CardTitle>Total Sales (Bar Chart)</CardTitle>
           </CardHeader>
           <CardContent>
-            <ResponsiveContainer width="100%" height={400} className=''>
-              <BarChart data={salesData}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="month" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="sales" fill="#2563eb" />
-              </BarChart>
+            <ResponsiveContainer width="100%" height={500}>
+              <BarNew/>
             </ResponsiveContainer>
           </CardContent>
         </Card>
@@ -122,14 +112,7 @@ export default function Home() {
           </CardHeader>
           <CardContent>
             <ResponsiveContainer width="100%" height={400}>
-              <LineChart data={revenueData}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="month" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
-              </LineChart>
+              <LineNew/>
             </ResponsiveContainer>
           </CardContent>
         </Card>
@@ -141,20 +124,7 @@ export default function Home() {
           </CardHeader>
           <CardContent>
             <ResponsiveContainer width="100%" height={400}>
-              <PieChart>
-                <Pie
-                  data={productSalesData}
-                  dataKey="value"
-                  cx="50%"
-                  cy="50%"
-                  outerRadius={150}
-                  label
-                >
-                  {productSalesData.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                  ))}
-                </Pie>
-              </PieChart>
+              <PieNew />
             </ResponsiveContainer>
           </CardContent>
         </Card>
@@ -164,20 +134,11 @@ export default function Home() {
           </CardHeader>
           <CardContent>
             <ResponsiveContainer width="100%" height={400}>
-              <ComposedChart data={inventoryData}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="inventory" barSize={20} fill="#413ea0" />
-                <Line type="monotone" dataKey="sales" stroke="#ff7300" />
-                <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
-              </ComposedChart>
+              <RadialChart />
             </ResponsiveContainer>
           </CardContent>
         </Card>
       </div>
     </div>
-  )
+  );
 }
